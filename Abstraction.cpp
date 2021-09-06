374ed4228026d2c52e68d666af167724e80ac6d9
Akriti <73133055+A-kriti@users.noreply.github.com>
2021-09-06 23:12:07 +0530

    Update Abstraction.cpp

--- Abstraction.cpp
+++ Abstraction.cpp
@@ -1,4 +1,3 @@
-
 /*Data abstraction is one of the most essential and important feature of object oriented programming in C++. Abstraction means displaying only essential information and hiding 
 the details. Data abstraction refers to providing only essential information about the data to the outside world, hiding the background details or implementation.*/
 
